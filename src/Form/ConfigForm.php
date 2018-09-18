32a8f531c3a2d949789e837a2ab476da022db7bd
David Bullmore <daveabullmore@gmail.com>
2018-09-18 17:07:07 +1000

    cleaned up a bunch of code, and now know how to append properties to a given entity type. Going through the whole form process now takes the chosen class and presents it to the user on submit as a drupal_message. Actual data to now be appended

--- src/Form/ConfigForm.php
+++ src/Form/ConfigForm.php
@@ -14,15 +14,34 @@ use Drupal\field\FieldConfigInterface;
 
 class ConfigForm extends FormBase{
 
-  public $ontology;
-  public $ontology_array;
+  // ontologyClass for functions and ontology array
+  public $ontology; // OntologyClass object
+  public $ontology_array; // Array of all Ontologies, with id, label, and desc.
 
+  // ontology arrays and variables needed
+  public $ontology_labels; // array of ontology labels used for lookup
+
+  // user chosen data
+  public $chosen_ontology; // user chosen ontology array[id, label, desc]
+  public $chosen_ontology_classes; // list of classes of a given ontology label
+  public $chosen_ontology_properties;
+  public $chosen_ontology_class; //
+  public $chosen_ontology_label; // user chosen ontology label
+
+  public $content_types; // 1D array of content type labels
+  public $chosen_content_type_label; // user chosen content type label
+  public $chosen_content_type_machine; // user chosen content type machine name
+  public $chosen_content_type_fields; // fields given a user chosen content type
+
+  // constructor
   public function __construct(){
     $this->ontology = new OntologyClass();
     $this->ontology_array = $this->ontology->getArray();
+    $this->ontology_labels = $this->ontology->getLabels();
+    $this->content_types = node_type_get_names();
   }
 
-  // handles the "next" button. // NEEDS FIXING
+  // handles the "next" button.
   public function nextSubmit(array &$form, FormStateInterface &$form_state) {
     $pageNum = $form_state->get('page_num');
     $prevPage = $pageNum-1;
@@ -65,6 +84,9 @@ class ConfigForm extends FormBase{
   // build form page 1
   public function buildForm(array $form, FormStateInterface $form_state){
 
+    //$testData = $this->getEntityMappings('article');
+    //var_dump($testData);
+
     // Display page 2 if $form_state->get('page_num') == 2.
     if ($form_state->has('page_num') && $form_state->get('page_num') == 2){
       return $this->buildFormPageTwo($form, $form_state);
@@ -90,7 +112,7 @@ class ConfigForm extends FormBase{
       '#description' => $this->t('Select the Content Type you want to begin mapping to'),
       '#type' => 'select',
       '#required' => TRUE,
-      '#options' => node_type_get_names(),
+      '#options' => $this->content_types,
     ];
 
     // ontology type drop down
@@ -99,7 +121,7 @@ class ConfigForm extends FormBase{
       '#description' => $this->t('Select the Ontology you want to use'),
       '#type' => 'select',
       '#required' => TRUE,
-      '#options' => $this->ontology->getLabels(),
+      '#options' => $this->ontology_labels,
     ];
 
     // next button
@@ -112,14 +134,6 @@ class ConfigForm extends FormBase{
       '#validate' => array(array($this, 'nextValidate')),
     ];
 
-/*
-    // DEBUGGING PURPOSES ONLy
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Calculate'),
-    ];
-*/
-
     return $form;
   }
 
@@ -128,35 +142,35 @@ class ConfigForm extends FormBase{
     // values of first page
     $value = $form_state->get(['page_values', 1]);
 
-    // get CT name
-    $ct_names = node_type_get_names();
-    $ct = $ct_names[$value['content-type']];
-    // get ont name
-    $ont_names = $this->ontology->getLabels();
-    $ont = $ont_names[$value['ontology-type']];
+    // set global data (chosen ont label, ont array, ont classes, properties, ct types)
+    $this->chosen_ontology_label = $this->ontology_labels[$value['ontology-type']];
+    $this->chosen_ontology = $this->ontology->getOntology($this->chosen_ontology_label);
+    $this->chosen_ontology_classes = $this->ontology->getClasses($this->chosen_ontology);
+    $this->chosen_ontology_properties = $this->ontology->getProperties($this->chosen_ontology);
 
-    // set ontArray and get class
-    $ontArray = $this->ontology->getOntology($ont);
-    $classes = $this->ontology->getClasses($ontArray);
+    $this->chosen_content_type_label = $this->content_types[$value['content-type']];
+    $this->chosen_content_type_machine = $value['content-type'];
+    $this->chosen_content_type_fields = $this->getFields($this->chosen_content_type_machine);
+
+    var_dump($this->chosen_ontology);
+    var_dump($this->ontology->getLabels());
 
     // top of form displaying current data
-
-    //top section
     $form['#title'] = $this->t('Semantic Map');
     $form['description'] = array(
       '#type' => 'item',
       '#title' => $this->t('Selected: '),
       '#description' =>
-      $this->t('Content Type: @CT', array('@CT' => $ct)) . '<br>' .
-      $this->t('Ontology: @ont', array('@ont' => $ont)),
+      $this->t('Content Type: @CT', array('@CT' => $this->chosen_content_type_label)) . '<br>' .
+      $this->t('Ontology: @ont', array('@ont' => $this->chosen_ontology_label)),
     );
 
     // class type drop down
     $form['class-type'] = [
-      '#title' => $this->t(' "@ont" Class Types', array('@ont' => $ont)),
+      '#title' => $this->t(' "@ont" Class Types', array('@ont' => $this->chosen_ontology_label)),
       '#description' => $this->t('Select the Ontology Class you want to use'),
-      '#type' => 'checkboxes',
-      '#options' => array_column($classes, 'label'),
+      '#type' => 'select',
+      '#options' => array_column($this->chosen_ontology_classes, 'label'),
         //'#default_value' => $form_state->getValue('rdf-type', ''),
     ];
 
@@ -184,24 +198,20 @@ class ConfigForm extends FormBase{
   }
 
   protected function buildFormPageThree(array $form, FormStateInterface $form_state){
-    // values from first page
-    $value = $form_state->get(['page_values', 1]);
+    // values from 2nd page
+    $value = $form_state->get(['page_values', 2]);
 
-    // get CT type names list, name, machineName and Fields
-    $ct_names = node_type_get_names();
-    $ct_name = $ct_names[$value['content-type']];
-    $ct_machine = $value['content-type'];
-    $ct_fields = $this->getFields($ct_machine);
+    // local array of class labels to set global chosen class
+    $classList = array_column($this->chosen_ontology_classes, 'label');
+    $this->chosen_ontology_class = $classList[$value['class-type']];
 
-    // get ont name
-    $ont_names = $this->ontology->getLabels();
-    $ont = $ont_names[$value['ontology-type']];
+    var_dump($this->chosen_ontology_class);
 
     // set ontArray and get properties
-    $ontArray = $this->ontology->getOntology($ont);
-    $properties = $this->ontology->getProperties($ontArray);
-    $property_labels = array_column($properties, 'label');
+    $property_labels = array_column($this->chosen_ontology_properties, 'label');
+    $property_descriptions = array_column($this->chosen_ontology_properties, 'description');
 
+    // top section
     $form['#title'] = $this->t('Semantic Map');
     $form['description'] = array(
       '#type' => 'item',
@@ -218,10 +228,10 @@ class ConfigForm extends FormBase{
       ),
     );
 
-    // Next, loop through the $ct_fields array array.length times,
+    // Next, loop through the $ct_fields array, array.length times,
     // add select dropdown of ont per ct_field, and enable box.
     // creates an associative array for each element
-    foreach ($ct_fields as $field) {
+    foreach ($this->chosen_content_type_fields as $field) {
       $table[$field] = array(
         'enable' => array(
           '#type' => 'checkbox',
@@ -229,7 +239,7 @@ class ConfigForm extends FormBase{
           '#title_display' => 'invisible',
         ),
         'ct_field' => array(
-          '#markup' => '<b>' . $field . '</b>' . '<br>' . $field .  $this->t(' description') . '</br>',
+          '#markup' => '<b>' . $field . '</b>' . '<br>' . $field . $this->t(' description') . '</br>',
           '#description' => $this->t('Select the Ontology Class you want to use'),
         ),
         'ont_property' => array(
@@ -247,7 +257,7 @@ class ConfigForm extends FormBase{
 
     // next button
     $form['actions'] = array('#type' => 'actions');
-    $form['actions']['next'] = [
+    $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Save'),
       '#button_type' => 'primary',
@@ -271,6 +281,9 @@ class ConfigForm extends FormBase{
 
   // returns fields for a given content-type (string)(ct_machineName)
   // THIS CAN BE MORE EFFICIENT. BUNDLE SHOULD = BUNDLEFIELDS I THINK
+
+  // THE ANSWER IS HERE FOR SAVING AND HANDLING THE DATA! NODE AND BUNDLE! USE MACHINE_NAME
+  // entityManager must be changed to entityTypeManager
   public function getFields(string $bundle){
     $entityManager = \Drupal::service('entity_field.manager');
     $entity_type_id = 'node';
@@ -292,6 +305,16 @@ class ConfigForm extends FormBase{
     return $newArr;
   }
 
+  public function getEntityMappings(string $bundle){
+    $entityManager = \Drupal::service('entity_field.manager');
+    $entity_type_id = 'node';
+    $mappings = rdf_get_mapping($entity_type_id, $bundle);
+
+    return $mappings;
+  }
+
+
+
   public function nextValidate(array $form, FormStateInterface $form_state) {
   // @TODO validate if required.
   }
@@ -303,12 +326,55 @@ class ConfigForm extends FormBase{
 
   // THIS IS TESTING ONLY ATM
   public function submitForm(array &$form, FormStateInterface $form_state){
-    $value = $form_state->getValue('ontology-type' , '#options');
-    $arr = $this->ontology->getLabels();
-    $val = $arr[$value];
-    $ont = $this->ontology->getOntology($val);
-    $id = $this->ontology->getId($ont);
 
-    drupal_set_message($id);
+    // page 1 variables
+    $page_one_values = $form_state->get(['page_values', 1]);
+    $content_type = $page_one_values['content-type'];
+    $ontology_type = $page_one_values['ontology-type'];
+
+    // ct_machine name
+    $ct_names = node_type_get_names();
+    $ct_name = $ct_names[$page_one_values['content-type']];
+    $ct_machine = $page_one_values['content-type'];
+
+    $ont_names = $this->ontology->getLabels();
+    $ont = $ont_names[$page_one_values['ontology-type']];
+
+    //page 2 variables
+    $page_two_values = $form_state->get(['page_values', 2]);
+
+    $ontArray = $this->ontology->getOntology($ont);
+    $classes = $this->ontology->getClasses($ontArray);
+    $ontology_classes = array_column($classes, 'label');
+
+    $ontology_class = $ontology_classes[$page_two_values['class-type']];
+
+    // page 3 variables
+
+    // DELETE ALL ABOVE??
+
+
+
+
+
+    // get mappings, given content type ct_machine
+    $mappings = $this->getEntityMappings($ct_machine);
+
+    $message = $this->t($this->chosen_ontology['id'] . ":" . $this->chosen_ontology_class);
+
+    //$mappings->setBundleMapping(array('types' => array($ontology_class)))->save();
+
+    // NEEDS UPDATE TO CHECK FOR name
+    // check name, if no name, add schema:name
+
+    // Add mapping for title field.
+
+
+
+    // iterate through enabled properties, and append them to $ontology_properties
+
+
+    //drupal_set_message(print_r($message, TRUE));
+    drupal_set_message($message);
   }
 }
